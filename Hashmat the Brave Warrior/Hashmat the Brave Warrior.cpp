69734a34f12b77853c827b99db75ccda9eadbc93
Sabbir <sabbir106706@gmail.com>
2020-05-06 20:47:08 +0600

    p1 using cpp

---
+++ Hashmat the Brave Warrior/Hashmat the Brave Warrior.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ long long a, b;
+
+ while(cin >> a >> b)
+ {
+  if(a < b){
+   cout << b - a << endl;
+  }else{
+   cout << a - b << endl;
+  }
+ }
+
+ return 0;
+}
+
