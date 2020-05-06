a78cc3cd19e86516c586169bc0ec1d3ffd77e4ad
Sabbir <sabbir106706@gmail.com>
2020-05-06 21:04:07 +0600

    p2 using cpp

---
+++ Back to High School Physics/Back to High School Physics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   long long v ,t ;
+   while(cin>>v>>t)
+   {
+       if ((-100<=v<=100) && (0<=t<=200))
+       {
+           cout<<v*t*2<<endl;
+
+       }
+
+   }
+
+}
