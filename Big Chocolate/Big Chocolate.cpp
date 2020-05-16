46e36118a26d50ac8231ff824c6c0784d27ea792
Sabbir <sabbir106706@gmail.com>
2020-05-17 02:31:41 +0600

    p13 using cpp

---
+++ Big Chocolate/Big Chocolate.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a,b;
+    while(scanf("%d %d", &a, &b) == 2)
+    {
+        printf("%d\n", a*b-1);
+
+    }
+
+    return 0;
+}
+
+
